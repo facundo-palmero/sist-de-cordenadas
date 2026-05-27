b285d7ce885d36e18526201c857baaa87a46db2a
facundo-palmero <facundopalmero082@gmail.com>
2026-05-27 06:22:08 +0000

    mi sist de cordenadas

---
+++ main.py
@@ -0,0 +1,16 @@
+cordenadas = [[i for i in range(4)]for j in range(4)]
+while True:
+    user_rta1 = int(input("ingresa un num de lista o escribe -10 para salir: "))
+    if user_rta1 == -10:
+        break
+    elif user_rta1 > 3:
+        print("se pasa de los limites")
+        continue
+    else:
+        user_rta2 = int(input("ok ahora escribe un num: "))
+        if user_rta2 > 3:
+            print("se pasas de los limites")
+            continue
+        total = cordenadas[user_rta1][user_rta2]
+        print(total)
+print("gracias por usar el sis de cordenadas")
